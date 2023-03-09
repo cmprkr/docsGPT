ae694e34f627b3b8fc441d5d8c178f0bbdf26c6e
Cam Park <cam@Cams-MacBook-Air.local>
2023-03-08 20:18:26 -0500

    added image

--- script.js
+++ script.js
@@ -1,9 +1,8 @@
-async function fetchData() {
-    const res=await fetch ("https://api.coronavirus.data.gov.uk/v1/data");
-    const record=await res.json();
-    document.getElementById("date").innerHTML=record.data[0].date;
-    document.getElementById("areaName").innerHTML=record.data[0].areaName;
-    document.getElementById("latestBy").innerHTML=record.data[0].latestBy;
-    document.getElementById("deathNew").innerHTML=record.data[0].deathNew;
-}
-fetchData();
+function essay_helper_toggle() {
+    var decider = document.getElementById('flexCheckDefault');
+    if(decider.checked){
+        alert('Essay Helper On');
+    } else {
+      alert('Essay Helper Off');
+    }
+};
